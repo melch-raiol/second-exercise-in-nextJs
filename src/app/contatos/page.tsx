6bb41dd531f1c1a3a2152499ddbd03c17758f856
melch-raiol <melchraiol@hotmail.com>
2024-05-04 12:11:03 -0300

    starting the project

---
+++ src/app/contatos/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Contatos(){
+    return(
+        <div>
+            <h1>Contatos</h1>
+            <span>(xx) 231-4390</span>
+        </div>
+    ) 
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,49 @@
+
+interface DataProps{
+  id: number;
+  name: string;
+  full_name: string;
+  owner:{
+    login: string;
+    id: number;
+    avatar_url: string;
+    url: string;
+  }
+}
+
+async function delayFetch(url: string, delay: number) {
+    await new Promise(resolve => setTimeout(resolve, delay))
+    const response = await fetch(url);
+    return response.json();
+}
+
+// async function getData() {
+//   const response = await fetch("https://api.github.com/users/melch-raiol/repos");
+
+//   return response.json()
+// }
+
+async function getData() {
+  const data = await delayFetch("https://api.github.com/users/melch-raiol/repos", 3500)
+  return data;
+}
+
+export default async function Home(){
+  const data: DataProps[] = await getData(); 
+
+  return(
+    <main>
+      <h1>Página Home</h1>
+      <span>Seja bem vindo a página home</span>
+      <br/>
+
+      <h3>Meus repositórios</h3>
+      {data.map( (item) =>(
+        <div key={item.id}>
+          <strong>Repositório:</strong><a>{item.name}</a>  
+          <br/>
+        </div>
+      ))}
+    </main>
+  )
+}
